c3d6562fd2a177528b80a9abd434dc482a0e139b
Helga front-end dev <67590093+Olga4000@users.noreply.github.com>
2020-07-07 13:17:57 +0500

    Add files via upload

---
+++ din.js
@@ -0,0 +1,7 @@
+var element = document.getElementById('color');
+element.onmouseover = function(){
+	this.style.background = 'purple';
+	this.style.height = '150px';
+	this.style.width = '150px';
+}
+
